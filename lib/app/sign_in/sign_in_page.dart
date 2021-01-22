2a19a95fdc512908c4e4349e501e394916e6f1a4
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-22 20:31:14 +0530

    Implement sign in page

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatelessWidget {
@@ -8,6 +7,41 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Time Tracker'),
         elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            )
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.redAccent,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
       ),
     );
   }
